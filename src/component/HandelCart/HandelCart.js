38bdd92f6f53adec93e19f2fa9781955fe981595
arifulislam10111999 <arifulislam10111999@gmail.com>
2021-09-26 19:26:51 +0600

    agin

--- src/component/HandelCart/HandelCart.js
+++ src/component/HandelCart/HandelCart.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import './HandelCart.css'
+
 
 const HandelCart = (props) => {
-
-   console.log(props.Cart);
-//    const {cart}=props.Cart;
-let total=0;
+  //const {cart}=props.Cart;
+let total=0; 
    for(const product of props.Cart){
-       console.log(product);
+       console.log(product.img);
        total=total+ product.salary
+       
 
      
    }
     return (
-        <div>
-            <h4><i class="fas fa-user"></i> Employer Added:{props.Cart.length} </h4>
-              <h6>Total Cost:{total} </h6>
+        
+            <div className="sideCart">
+                <h3>Monthly cost Report cart :- </h3>
+            <h4 className="pt-5"><i class="fas fa-user"></i> Employer Added:{props.Cart.length} </h4>
+              <h6 className="pt-5">Total Monthly Salary:{total} </h6>
         </div>
+        
+        
     );
 };
 
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css';
 const Header = () => {
     return (
         <div  className="headerStyle pt-5 pb-5">
-            <h1 className="fw-bold">The Multinational IT company  </h1>
-            <p>We are  provide  best service and making a beautiful website for you !</p>
+            <h1 className="fw-bold ">The Multinational IT company  </h1>
+            <p className="text-black-50">We are  provide  best service and making a beautiful website for you !</p>
             <h3>Total Budget :100 million</h3>
         </div>
     );
